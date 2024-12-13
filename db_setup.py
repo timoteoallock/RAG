247867687142adb50be202819b1f4f8282b8d9ed
Skald Somson <91849465+timoteoallock@users.noreply.github.com>
2024-12-13 12:59:36 +0100

    Update db_setup.py

--- db_setup.py
+++ db_setup.py
@@ -5,7 +5,6 @@ import pandas as pd
 def setup_chroma_db(collection_name="medical_qa_collection", max_rows=15000, persist_path = ""):
     
     data = pd.read_csv('path\to\dataset\')
-    data = data.to_pandas()
     data["id"] = data.index
     
     
