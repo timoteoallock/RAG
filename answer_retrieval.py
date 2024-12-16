006a4fe5442258c126a3dd2a5063b50218d0e8be
Changchen Yu <91849465+timoteoallock@users.noreply.github.com>
2024-12-16 07:01:13 +0100

    Update answer_retrieval.py

--- answer_retrieval.py
+++ answer_retrieval.py
@@ -1,4 +1,3 @@
-
 import chromadb
 from ragatouille import RAGPretrainedModel
 
@@ -7,14 +6,13 @@ def query_database(question, collection_name="medical_qa_collection", persist_pa
     chroma_client = chromadb.PersistentClient(path=persist_path)
     collection = chroma_client.get_collection(name=collection_name)
     
-    # Query Chroma collection based on the input question
+    # Query Chroma collection 
     results = collection.query(query_texts=[question], n_results=n_results)
-    relevant_docs = results["documents"]
+    relevant_docs = results["documents"][0]
 
-    # Load the reranker model (if reranking is enabled)
+    # Load and apply the reranker model (if reranking is enabled)
     reranker = RAGPretrainedModel.from_pretrained("colbert-ir/colbertv2.0") if rerank else None
     
-    # Apply reranking if the reranker is provided
     if reranker:
         reranked_docs = reranker.rerank(question, relevant_docs, k=num_docs_final)
         # Extract the final content of the reranked documents
